2148e24d38296fce09a4e1af7a44493e45df2cd7
SamirHodzic <samir.sgd@gmail.com>
2016-09-21 10:36:26 +0200

    - init select single/multiple type partial

--- app/models/post_field.rb
+++ app/models/post_field.rb
@@ -2,6 +2,6 @@ class PostField < ApplicationRecord
   belongs_to :post_definition
 
   def self.all_types
-    return ['text', 'text_area', 'number', 'date', 'time', 'date_time']
+    return ['text', 'text_area', 'number', 'select_single', 'select_multiple', 'date', 'time', 'date_time']
   end
 end
