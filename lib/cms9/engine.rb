c7085bed15d257fcde1907667414b3e940691281
SamirHodzic <samir.sgd@gmail.com>
2016-09-23 12:25:22 +0200

    - fix engine.rb 4th

--- lib/cms9/engine.rb
+++ lib/cms9/engine.rb
@@ -8,14 +8,5 @@ module Cms9
             end
         end
     end
-
-    original_js = File.binread("app/assets/javascripts/application.js")
-    if original_js.include?("require 'cms9'")
-       say_status("skipped", "insert into app/assets/javascripts/application.js", :yellow)
-    else
-       insert_into_file "app/assets/javascripts/application.js", :after => %r{//= require ['"]?jquery['"]?} do
-          "\n//= require 'cms9'\n\n"
-       end
-    end
   end
 end
