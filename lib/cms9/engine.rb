d26e28a70e3c457882f4e153b848a0c20dcbd908
SamirHodzic <samir.sgd@gmail.com>
2016-09-23 11:45:32 +0200

    - fix engine.rb

--- lib/cms9/engine.rb
+++ lib/cms9/engine.rb
@@ -18,15 +18,5 @@ module Cms9
       end
     end
 
-    initialize "team_page.load_app_instance_data" do |app|
-      TeamPage.setup do |config|
-        config.app_root = app.root
-      end
-    end
-
-    initialize "team_page.load_static_assets" do |app|
-      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
-    end
-
   end
 end
