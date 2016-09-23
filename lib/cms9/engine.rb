9001eaf6832e1cb887fd894a76813bc6250b0763
SamirHodzic <samir.sgd@gmail.com>
2016-09-23 12:30:48 +0200

    - fix engine.rb 8th

--- lib/cms9/engine.rb
+++ lib/cms9/engine.rb
@@ -1,6 +1,14 @@
 module Cms9
   class Engine < ::Rails::Engine
     isolate_namespace Cms9
+    require 'sass-rails'
+    require 'uglifier'
+    require 'coffee-rails'
+    require 'jquery-rails'
+    require 'jbuilder'
+    require 'bootstrap'
+    require 'wysiwyg-rails'
+
     initializer :append_migrations do |app|
         unless app.root.to_s.match(root.to_s)
             config.paths["db/migrate"].expanded.each do |expand_path|
