1d740d1fb9ceb6eae3e87ea0a5d86045c7d66541
SamirHodzic <samir.sgd@gmail.com>
2016-09-23 11:44:25 +0200

    - test js/scss include and auto mount route 3nd

--- lib/cms9/engine.rb
+++ lib/cms9/engine.rb
@@ -9,16 +9,24 @@ module Cms9
         end
     end
 
-    paths["app"]                 # => ["app"]
-    paths["app/controllers"]     # => ["app/controllers"]
-    paths["app/helpers"]         # => ["app/helpers"]
-    paths["app/models"]          # => ["app/models"]
-    paths["app/views"]           # => ["app/views"]
-    paths["lib"]                 # => ["lib"]
-    paths["lib/tasks"]           # => ["lib/tasks"]
-    paths["config"]              # => ["config"]
-    paths["config/initializers"] # => ["config/initializers"]
-    paths["config/locales"]      # => ["config/locales"]
-    paths["config/routes.rb"]    # => ["config/routes.rb"]
+    config.Cms9 = ActiveSupport::OrderedOptions.new
+    initializer 'cms9.configuration' do |app|
+      if app.config.my_engine[:mounted_path]
+        app.routes.append do
+          mount Cms9::Engine => app.config.cms9[:mounted_path]
+        end
+      end
+    end
+
+    initialize "team_page.load_app_instance_data" do |app|
+      TeamPage.setup do |config|
+        config.app_root = app.root
+      end
+    end
+
+    initialize "team_page.load_static_assets" do |app|
+      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
+    end
+
   end
 end
