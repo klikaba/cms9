cb952b91e6844876ddbe058668fa2932927a373f
Zaharije Pasalic <pasalic.zaharije@gmail.com>
2016-10-09 11:50:06 +0200

    Experimenting with data interface

--- app/models/cms9/post.rb
+++ app/models/cms9/post.rb
@@ -10,5 +10,6 @@ module Cms9
       post_field = Cms9::PostField.where(name: name, post_definition: self.post_definition).first
       return Cms9::Field.where(post: self, post_field: post_field).first
     end
+
   end
 end
--- app/models/cms9/post_definition.rb
+++ app/models/cms9/post_definition.rb
@@ -4,5 +4,11 @@ module Cms9
               length: { minimum: 3 }
 
     has_many :post_fields, dependent: :destroy
+    has_many :posts, dependent: :destroy
+
+
+    def self.find_type(name) 
+      return PostDefinition.where(name: name).first
+    end
   end
 end
