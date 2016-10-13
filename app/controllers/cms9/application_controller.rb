b9cc1f7b6755b5b2938acc4cb53b1fda108064f6
SamirHodzic <samir.sgd@gmail.com>
2016-10-13 20:52:18 +0200

    - update authorize part in applicationController

--- app/controllers/cms9/application_controller.rb
+++ app/controllers/cms9/application_controller.rb
@@ -7,39 +7,25 @@ module Cms9
 
     def current_user
       current_user_ident = Cms9.configuration.current_user || :current_user
+      parent = ::ApplicationController.new
+      parent.request = request
 
-      parent = parent_controller
       if parent.respond_to?(current_user_ident)
         parent.send(current_user_ident)
-      else
-        return Cms9::User.new
       end
     end
 
     def authorize
-      unless current_user.cms9_admin?
+      unless current_user.try(:cms9_admin?)
+        Rails.logger.error "\n  *************************************************************************"
+        Rails.logger.error "  *                                                                       *"
+        Rails.logger.error "  *  \033[31mYOUR CMS IS OPENED FOR EDITING!!!\033[0m                                    *"
+        Rails.logger.error "  *  \033[31mPLEASE IMPLEMENT cms9_admin? METHOD FOR current_user!\033[0m                *"
+        Rails.logger.error "  *                                                                       *"
+        Rails.logger.error "  *************************************************************************\n"
+
         redirect_to '/'
       end
-      @missing_implementation = current_user.instance_of?(Cms9::User)
-      Rails.logger.error "\n  *************************************************************************"
-      Rails.logger.error "  *                                                                       *"
-      Rails.logger.error "  *  \033[31mYOUR CMS IS OPENED FOR EDITING!!!\033[0m                                    *"
-      Rails.logger.error "  *  \033[31mPLEASE IMPLEMENT cms9_admin? METHOD FOR current_user!\033[0m                *"
-      Rails.logger.error "  *                                                                       *"
-      Rails.logger.error "  *************************************************************************\n"
-    end
-
-    private
-      def parent_controller
-        controller = ::ApplicationController.new
-        controller.request = request
-      end
-  end
-
-  class User
-    def cms9_admin?
-      return true
     end
   end
-
 end
